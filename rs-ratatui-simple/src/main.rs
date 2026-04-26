baaa6b0f532f10682c511fbdcc45334dd1e38283
jbm950 <jmilam343@gmail.com>
2026-04-26 14:49:30 -0400

    rs-ratatui-simple: refactor
    This commit refactors the 'simple' example making it quite a bit more
    verbose by now the example showcases the different hooks/spots that
    could be expanded upon which were not as obvious in the original. The
    application state, event loop and UI drawing widgets can now be seen as
    much more distinct steps.
    
    Refs:

--- rs-ratatui-simple/src/main.rs
+++ rs-ratatui-simple/src/main.rs
@@ -1,21 +1,42 @@
-use ratatui::{DefaultTerminal, Frame};
+use ratatui::{DefaultTerminal, Frame, widgets::Paragraph};
+
+
+struct App {
+    pub my_hello_text: String,
+}
+
+
+impl App {
+    fn new() -> App {
+        App {
+            my_hello_text: String::from("Hello World"),
+        }
+    }
+}
 
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
-    ratatui::run(app)?;
+    let mut app = App::new();
+    ratatui::run(|terminal| {run_app(terminal, &mut app)})?;
     Ok(())
 }
 
-fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+
+fn run_app(terminal: &mut DefaultTerminal, app: &mut App) -> std::io::Result<()> {
     loop {
-        terminal.draw(render)?;
+        // Redraw each frame
+        terminal.draw(|frame| {ui(frame, app)})?;
+
+        // Event Loop
         if crossterm::event::read()?.is_key_press() {
             break Ok(());
         }
     }
 }
 
-fn render(frame: &mut Frame) {
-    frame.render_widget("hello world", frame.area());
+
+fn ui(frame: &mut Frame, app: &App) {
+    let hello_widget = Paragraph::new(app.my_hello_text.clone());
+    frame.render_widget(hello_widget, frame.area());
 }
