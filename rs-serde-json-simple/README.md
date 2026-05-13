80f557a7860393b31ea4708921cab63f3512de40
jbm950 <jmilam343@gmail.com>
2026-05-12 21:08:15 -0400

    rs-serde-json-simple: extend with custom struct
    This commit extends the simple example with a custom struct since it was
    pretty easy/straightforward.
    
    Refs:

--- rs-serde-json-simple/README.md
+++ rs-serde-json-simple/README.md
@@ -1,2 +1,3 @@
 Simple example showing how Serde JSON can convert a json string into a useable
-general data structure.
+general data structure. The same string is also converted to a custom data
+structure.
--- rs-serde-json-simple/src/main.rs
+++ rs-serde-json-simple/src/main.rs
@@ -1,4 +1,12 @@
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
+
+#[derive(Serialize, Deserialize)]
+struct Character {
+    name: String,
+    level: u8,
+    weapons: Vec<String>
+}
 
 fn main() -> std::io::Result<()> {
     let data = r#"
@@ -17,6 +25,13 @@ fn main() -> std::io::Result<()> {
     println!(
         "Character {} is level {} and his first weapon is {}",
         v["name"], v["level"], v["weapons"][0]
+    );
+
+    let c: Character = serde_json::from_str(data)?;
+
+    println!(
+        "Character \"{}\" is level {} and his first weapon is \"{}\"",
+        c.name, c.level, c.weapons[0]
     );
 
     Ok(())
