5054dbe94cab64630748a227e669774aaad3da41
jbm950 <jmilam343@gmail.com>
2026-06-25 17:53:51 -0400

    rs-proj-1553-send-data: initial commit
    This commit brings in the library files from rs-proj-1553-heartbeat as
    an initial set of code for this example. The code is being pulled
    between examples and built upon as I go.
    
    Refs:

---
+++ rs-proj-1553-send-data/src/lib.rs
@@ -0,0 +1,4 @@
+pub mod bus;
+pub mod bus_controller;
+pub mod protocol;
+pub mod rt;
---
+++ rs-proj-1553-send-data/src/protocol/mod.rs
@@ -0,0 +1,6 @@
+mod cmd_word;
+mod status_word;
+mod support;
+
+pub use cmd_word::{CmdWord, Subaddress, TxRx};
+pub use status_word::StatusWord;
