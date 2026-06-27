19b8ab5a29e53fd49fb89657d1d099b97dd74fb3
jbm950 <jmilam343@gmail.com>
2026-06-27 08:18:58 -0400

    rs-proj-1553-send-data: message struct initial commit

--- rs-proj-1553-send-data/src/protocol/mod.rs
+++ rs-proj-1553-send-data/src/protocol/mod.rs
@@ -1,10 +1,12 @@
 mod cmd_word;
 mod data_word;
+mod message;
 mod protocol_word;
 mod status_word;
 mod support;
 
 pub use cmd_word::{CmdWord, Subaddress, TxRx};
 pub use data_word::DataWord;
+pub use message::{CommandMessage, StatusMessage};
 pub use protocol_word::ProtocolWord;
 pub use status_word::StatusWord;
